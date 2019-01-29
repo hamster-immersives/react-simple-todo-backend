deb51c98371645949e0737f48e63a105e6cd595c
pak <pak@1916.lan>
2019-01-29 12:56:26 -0500

    first

---
+++ controllers/userController.js
@@ -0,0 +1,107 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+
+    registerAndSignUp: function(params) {
+        return new Promise(function(resolve, reject) {
+            User
+                .findOne({email: params.email})
+                .then(user => {
+                    if (user) {
+                        bcrypt
+                            .compare(params.password, user.password)
+                            .then(isMatch => {
+
+                                if (isMatch) {
+
+                                    const payload = {
+                                        id: user._id,
+                                        email: user.email
+                                    }
+
+                                    jwt.sign(payload, process.env.SECRET_KEY, {
+                                        expiresIn: 3600
+                                    }, (err, token) => {
+
+                                        if (err) {
+                                            reject(err)
+                                        } else {
+                                            let success = {};
+                                            success.confirmation = true;
+                                            success.loggedIn = "Logged In"
+                                            success.token = 'Bearer ' + token;
+                                            resolve(success);
+                                        }
+
+                                    })
+
+                                } else {
+                                    let errors = {}
+                                    errors.password = "Password incorrect";
+                                    errors.status = 400;
+                                    reject(err)
+                                }
+
+                            })
+
+                    } else {
+
+                        const newUser = new User({email: params.email, password: params.password});
+
+                        bcrypt.genSalt(10, (err, salt) => {
+                            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                                if (err) {
+                                    reject(err)
+                                } else {
+
+                                    newUser.password = hash;
+
+                                    newUser
+                                        .save()
+                                        .then(user => {
+
+                                            const payload = {
+                                                id: user._id,
+                                                email: user.email
+                                            }
+
+                                            jwt.sign(payload, process.env.SECRET_KEY, {
+                                                expiresIn: 3600
+                                            }, (err, token) => {
+
+                                                if (err) {
+                                                    reject(err)
+                                                } else {
+                                                    let success = {};
+                                                    success.confirmation = true;
+                                                    success.signup = "signed up"
+                                                    success.token = 'Bearer ' + token;
+                                                    resolve(success)
+                                                }
+
+                                            })
+
+                                        })
+                                        .catch(err => {
+                                            reject(err)
+                                        })
+
+                                }
+                            })
+                        })
+
+
+
+                    }
+
+                })
+                .catch(err => {
+
+                })
+
+        });
+    }
+
+}
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport')
+
+var userController = require('../controllers/userController');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('YOU HAVE HIT THIS ENDPOINT');
+});
+
+router.post('/auth', function(req, res, next) {
+  userController.registerAndSignUp(req.body)
+    .then(user => {
+      res.json(user)
+    })
+    .catch(err => {
+      res.json(err)
+    })
+});
+
+router.get('/currentuser', passport.authenticate('jwt', {session: false}), function(req, res) {
+  res.json({
+    id: req.user.id,
+    email: req.user.email
+  })
+})
+
+module.exports = router;
